60e34758bc1cf6e06d74004fc7f90b3631470e2c
OwenNolis <83425846+OwenNolis@users.noreply.github.com>
2023-12-20 21:03:17 +0100

    Try-out spritesheet with multiple sprites
    MilitaryRobot DeathBeam sprite with Spwan of DeathBeam and DeathBeam itself

---
+++ Game1.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace DoomedOfTheDeath
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D texture;
+        private Texture2D texture_MilitaryRobot_DeathBeamAttack;
+        private Texture2D texture_MilitaryRobot_DeathBeam_Spawn;
+        private Texture2D texture_MilitaryRobot_DeathBeam;
+
+        Hero hero;
+        Hero militaryRobot_DeathBeamAttack;
+        Hero militaryRobot_DeathBeam_Spawn;
+        Hero militaryRobot_DeathBeam;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+
+            hero = new Hero(texture);
+            militaryRobot_DeathBeamAttack = new Hero(texture_MilitaryRobot_DeathBeamAttack);
+            militaryRobot_DeathBeam_Spawn = new Hero(texture_MilitaryRobot_DeathBeam_Spawn);
+            militaryRobot_DeathBeam = new Hero(texture_MilitaryRobot_DeathBeam);
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            // TODO: use this.Content to load your game content here
+            texture = Content.Load<Texture2D>("_textureMilitaryRobot_Movement");
+            texture_MilitaryRobot_DeathBeamAttack = Content.Load<Texture2D>("_textureMilitaryRobot_DeathBeamAttack");
+            texture_MilitaryRobot_DeathBeam_Spawn = Content.Load<Texture2D>("_textureMilitaryRobot_DeathBeam_Spawn");
+            texture_MilitaryRobot_DeathBeam = Content.Load<Texture2D>("_textureMilitaryRobot_DeathBeam");
+
+
+            InitializeGameObjects();
+        }
+
+        private void InitializeGameObjects()
+        {
+            hero = new Hero(texture);
+            militaryRobot_DeathBeamAttack = new Hero(texture_MilitaryRobot_DeathBeamAttack);
+            militaryRobot_DeathBeam_Spawn = new Hero(texture_MilitaryRobot_DeathBeam_Spawn);
+            militaryRobot_DeathBeam = new Hero(texture_MilitaryRobot_DeathBeam);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            hero.Update(gameTime);
+            militaryRobot_DeathBeamAttack.Update(gameTime);
+            militaryRobot_DeathBeam_Spawn.Update(gameTime);
+            militaryRobot_DeathBeam.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            // TODO: Add your drawing code here
+
+            GraphicsDevice.Clear(Color.DarkGray);
+
+            _spriteBatch.Begin();
+
+            hero.Draw(_spriteBatch);
+            militaryRobot_DeathBeamAttack.Draw(_spriteBatch);
+            militaryRobot_DeathBeam_Spawn.Draw(_spriteBatch);
+            militaryRobot_DeathBeam.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+            
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Hero.cs
@@ -0,0 +1,49 @@
+﻿using DoomedOfTheDeath.Animations;
+using DoomedOfTheDeath.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoomedOfTheDeath
+{
+    public class Hero : IGameObject
+    {
+        Texture2D heroTexture;
+        Animation animation;
+
+        public Hero(Texture2D _textureMilitaryRobot)
+        {
+            heroTexture = _textureMilitaryRobot;
+            animation = new Animation();
+            /*animation.AddFrame(new AnimationFrame(new Rectangle(0, 0, 400, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(160, 0, 400, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(320, 0, 400, 96)));
+            animation.AddFrame(new AnimationFrame(new Rectangle(480, 0, 400, 96)));*/
+
+            // Animation for Movement
+            //animation.GetFramesFromTextureProperties(_textureMilitaryRobot.Width, _textureMilitaryRobot.Height, 4, 1);
+
+            // Animation for Shield
+            //animation.GetFramesFromTextureProperties(_textureMilitaryRobot.Width, _textureMilitaryRobot.Height, 10, 1);
+
+            // Animation for DeathBeamAttack
+            animation.GetFramesFromTextureProperties(_textureMilitaryRobot.Width, _textureMilitaryRobot.Height, 5, 1);
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            animation.Update(gameTime);
+        }
+        
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(heroTexture, new Vector2(300, 150), animation.CurrentFrame.SourceRectangle, Color.White);
+        }
+    }
+}

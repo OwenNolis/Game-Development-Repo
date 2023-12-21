c6d2d51a940b0dd09f2a1fb00263f25eaee0e0aa
OwenNolis <83425846+OwenNolis@users.noreply.github.com>
2023-12-21 21:57:48 +0100

    Testing of SpriteMovement
    New testing of the sprite movement so the sprite doesn't move from left ro right, instead the sprite stands still making it easier to move it.
    This for moving it later with the keyboard integration.

---
+++ TestSpriteMovement/TestSpriteMovement/Game1.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestSpriteMovement
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D texture;
+        private Rectangle partRectangle;
+        private int moveOn_X = 0;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            // CharacterSheet
+            //partRectangle = new Rectangle(moveOn_X, 0,180, 248);
+
+            // MilitaryRobot
+            partRectangle = new Rectangle(moveOn_X, 0, 75, 81);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // CharacterSheet
+            //texture = Content.Load<Texture2D>("CharacterSheet");
+
+            // MilitaryRobot
+            texture = Content.Load<Texture2D>("_textureMilitaryRobot_Movement");
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.BurlyWood);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            // CharacterSheet
+            //_spriteBatch.Draw(texture, new Vector2(10,10), partRectangle, Color.White);
+
+            // MilitaryRobot
+            _spriteBatch.Draw(texture, new Vector2(10,10), partRectangle, Color.White);
+
+            _spriteBatch.End();
+
+            // CharacterSheet
+            /*
+            moveOn_X += 180;
+            if(moveOn_X > 900)
+            {
+                moveOn_X = 0;
+            }
+            partRectangle.X = moveOn_X;
+            */
+
+            // MilitaryRobot
+
+            moveOn_X += 80;
+            if (moveOn_X > 305)
+            {
+                moveOn_X = 0;
+            }
+            partRectangle.X = moveOn_X;
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ TestSpriteMovement/TestSpriteMovement/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new TestSpriteMovement.Game1();
+game.Run();
